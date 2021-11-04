97bb6463263ad38a0ed65a1b5f61136152d55d8a
Elias <98ille@gmail.com>
2021-11-04 15:58:10 +0100

    Added LOBSTER

--- Labb2/main.py
+++ Labb2/main.py
@@ -10,39 +10,50 @@ reader.SetDataExtent(0, 255, 0, 255, 0, 177)
 reader.SetDataScalarTypeToUnsignedChar()
 reader.Update()
 
-# Create volume mapper
-volumeMap = vtk.vtkSmartVolumeMapper()
-volumeMap.SetInputData(reader.GetOutput())
+output = reader.GetOutput()
 
-# Create volume
-volume = vtk.vtkVolume()
-volume.SetMapper(volumeMap)
-
-# Create contour filter using reader
 contour = vtk.vtkContourFilter()
 contour.SetInputConnection(reader.GetOutputPort())
-contour.GenerateValues(20, 0, 255)   # numContours, rangeStart, rangeEnd
+contour.GenerateValues(10, 0, 255)
 
-# Create mapper using contour filter
 modelToContour = vtk.vtkPolyDataMapper()
 modelToContour.SetInputConnection(contour.GetOutputPort())
-
-# Create contour actor using mapper
 contourActor = vtk.vtkActor()
 contourActor.SetMapper(modelToContour)
 
-# Window
-window = vtk.vtkRenderWindow()
-window.SetSize(1000, 1000)
-interactor = vtk.vtkRenderWindowInteractor()
+#LOBSTER
 
-# contour
+contour2 = vtk.vtkContourFilter()
+contour2.SetInputConnection(reader.GetOutputPort())
+contour2.GenerateValues(40, 0, 255)
+
+plane = vtk.vtkPlane()
+plane.SetOrigin(79.51763853353643, 142.3562725373262, 113.45252769160768)
+plane.SetNormal(0.8645083127426199, 0.2248326171531122, -0.44952827660002725)
+
+clip = vtk.vtkClipPolyData()
+clip.SetInputConnection(contour2.GetOutputPort())
+clip.SetClipFunction(plane)
+clip.SetValue(0)
+clip.Update()
+
+contourToSlice = vtk.vtkPolyDataMapper()
+contourToSlice.SetInputConnection(clip.GetOutputPort())
+lobster = vtk.vtkActor()
+lobster.SetMapper(contourToSlice)
+lobster.RotateY(135)
+lobster.RotateX(-35)
+
+
+reWin = vtk.vtkRenderWindow()
+reWin.SetWindowName("Labb 2")
+reWin.SetSize(800, 600)
+iren = vtk.vtkRenderWindowInteractor()
+
 renderer = vtk.vtkRenderer()
-renderer.SetBackground(.2, .2, .2 ) #Change background color
-renderer.AddActor(contourActor)
-window.AddRenderer(renderer)
+renderer.AddActor(lobster)
 
-# Start
-interactor.SetRenderWindow(window)
-window.Render()
-interactor.Start()
+reWin.AddRenderer(renderer)
+iren.SetRenderWindow(reWin)
+reWin.Render()
+iren.Start()
